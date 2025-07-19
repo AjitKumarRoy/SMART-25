e63fd8770fe4f77158c1cfd294bf371c04e6c481
AjitKumarRoy <ak47riderji@gmail.com>
2025-07-19 13:45:48 +0530

    next.config.ts modified

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    output: 'export',
+     basePath: '/~amdcg',
+  assetPrefix: '/~amdcg',
   /* config options here */
    images: {
     domains: [
